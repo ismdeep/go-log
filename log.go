14abfc142424ef771376e70b63dc39ca48fc7508
L. Jiang <l.jiang.1024@gmail.com>
2021-12-30 17:23:55 +0800

    refactor: disable show caller

--- log.go
+++ log.go
@@ -16,6 +16,8 @@ func init() {
 	}
 	cfg.EncoderConfig = zap.NewProductionEncoderConfig()
 	cfg.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+	cfg.DisableCaller = true
+	cfg.DisableStacktrace = false
 	logger, _ = cfg.Build()
 
 	defer func(logger *zap.Logger) {
@@ -46,4 +48,16 @@ func Panic(msg string, keysAndValues ...interface{}) {
 
 func Fatal(msg string, keysAndValues ...interface{}) {
 	logger.Sugar().Fatalw(msg, keysAndValues...)
+}
+
+func String(key string, value string) zap.Field {
+	return zap.String(key, value)
+}
+
+func FieldErr(err error) zap.Field {
+	return zap.Error(err)
+}
+
+func Any(key string, value interface{}) zap.Field {
+	return zap.Any(key, value)
 }
